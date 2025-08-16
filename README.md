dc4ea5308683d7a09afb18927ad005fe7ff5418c
Chris Engelhart <engleman@gmail.com>
2025-08-16 14:16:29 -0400

    First read only random version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # simpler-objects
 A simpler object storage service
+
+## Start storage servers
+
+For each filesystem/drive
+
+```
+python -m http.server -d /path/to/objects 46579
+```
+
+## Start object locator
+
+```
+OBJECT_SERVERS="http://localhost:46579/" fastapi dev --port 46752 simpler_objects/locator_api.py
+```
+
+## ObjectIndex
+
+Optionally on ports 46569 (API) and 46567 (GUI)
---
+++ simpler_objects/locator_api.py
@@ -0,0 +1,30 @@
+"""Simpler Objects Locator API"""
+
+import os
+import random
+import requests
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+OBJECT_SERVERS = os.environ.get('OBJECT_SERVERS', 'http://localhost:46579/')
+
+def object_servers(randomized=False):
+    """Return a randomized list of object server URLs"""
+    servers = OBJECT_SERVERS.split(',')
+    if randomized:
+        random.shuffle(servers)
+    return servers
+
+@app.get("/{object_path}")
+def find_object(object_path: str):
+    """Return a redirect to an existing object"""
+    for server in object_servers(randomized=True):
+        try:
+            result = requests.head(server + object_path, timeout=1)
+            result.raise_for_status()
+        except requests.exceptions.RequestException:
+            continue
+        return RedirectResponse(url=server+object_path)
+    raise HTTPException(status_code=404)
