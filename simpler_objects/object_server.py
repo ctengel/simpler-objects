e0abdd0ed48bc73173e863649a48f42c1545ba2f
ctengel <50063864+ctengel@users.noreply.github.com>
2026-01-03 16:59:32 -0500

    Rewrite object server as FastAPI
    Gains Range request ability in #2

--- simpler_objects/object_server.py
+++ simpler_objects/object_server.py
@@ -1,183 +1,121 @@
 """Simpler Objects Server"""
 
-# based on https://gist.github.com/fabiand/5628006
-
-import argparse
 import pathlib
-import io
-import json
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler, HTTPStatus
 import shutil
 import base64
 import hashlib
+import os
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import FileResponse, Response
 
+app = FastAPI()
+
+# TODO allow spec on command line
+OBJECT_DIRECTORY = os.environ.get('OBJECT_DIRECTORY', '.')
 BUFFER = 67108864
 
-class PutHTTPRequestHandler(SimpleHTTPRequestHandler):
-    """Extension to basic GET handler that also handles PUT"""
+@app.get('/health')
+def healthcheck():
+    """Return basic info on node health"""
+    disk_stats = shutil.disk_usage(pathlib.Path(OBJECT_DIRECTORY))
+    r = {'read': True,
+         'write': True,
+         'available': disk_stats.free,
+         'percent': int(float(disk_stats.free)/float(disk_stats.total)*100.0)}
+    return r
 
-    def do_PUT(self):
-        """Handle PUT requests"""
+@app.get("/{bucket}/{key}")
+def get_object(bucket: str, key: str):
+    """Handle GET requests"""
+    # TODO make this safer
+    return FileResponse(pathlib.Path(OBJECT_DIRECTORY).joinpath(bucket).joinpath(key))
 
-        # no PUT /health
-        if self.path == '/health':
-            self.send_response(403)
-            self.end_headers()
-            return
+@app.put("/{bucket}/{key}")
+def put_object(bucket: str, key: str, request: Request):
+    """Handle PUT requests"""
 
-        # parse Content-Length
-        try:
-            length = int(self.headers["Content-Length"])
-        except TypeError:
-            self.send_response(400)
-            self.end_headers()
-            return
+    # parse Content-Length
+    try:
+        length = int(request.headers["Content-Length"])
+    except TypeError as exc:
+        raise HTTPException(status_code=400) from exc
 
-        # ensure unique fikename
-        path = pathlib.Path(self.translate_path(self.path))
-        if path.exists():
-            self.send_response(409)
-            self.end_headers()
-            return
+    # ensure unique fikename
+    # TODO make this safer
+    path = pathlib.Path(OBJECT_DIRECTORY).joinpath(bucket).joinpath(key)
+    if path.exists():
+        raise HTTPException(status_code=409)
 
-        # parse Digest
-        request_digest = None
-        digest_header = self.headers.get('Repr-Digest')
-        if digest_header:
-            request_digest = base64.b64decode([x.partition('=')[2]
-                                               for x in digest_header.split(',')
-                                               if x.partition('=')[0] == 'sha-256'][0].strip(':'))
+    # parse Digest
+    request_digest = None
+    digest_header = request.headers.get('Repr-Digest')
+    if digest_header:
+        request_digest = base64.b64decode([x.partition('=')[2]
+                                           for x in digest_header.split(',')
+                                           if x.partition('=')[0] == 'sha-256'][0].strip(':'))
 
-        # Receive file
-        pos = 0
-        with open(path, "wb") as dst:
-            while True:
-                nextread = min(BUFFER, length - pos)
-                if not nextread:
-                    break
-                dst.write(self.rfile.read(nextread))
-                pos = pos + nextread
-        assert path.stat().st_size == length
+    # Receive file
+    with open(path, "wb") as dst:
+        # TODO async? check length?
+        for chunk in request.stream():
+            dst.write(chunk)
+    assert path.stat().st_size == length
 
-        # Hash and compare
-        hash_sha256 = hashlib.sha256()
-        with open(path, "rb") as f:
-            for chunk in iter(lambda: f.read(BUFFER), b""):
-                hash_sha256.update(chunk)
-        file_digest = hash_sha256.digest()
-        if request_digest and file_digest != request_digest:
-            self.send_response(400)
-            self.end_headers()
-            path.unlink()
-            return
+    # Hash and compare
+    hash_sha256 = hashlib.sha256()
+    with open(path, "rb") as f:
+        for chunk in iter(lambda: f.read(BUFFER), b""):
+            hash_sha256.update(chunk)
+    file_digest = hash_sha256.digest()
+    if request_digest and file_digest != request_digest:
+        path.unlink()
+        raise HTTPException(status_code=400)
 
-        # write hash to disk
-        bucket = path.parent
-        hash_file = bucket.parent.joinpath(bucket.name).with_suffix('.sha256')
-        cksum_line = f"{file_digest.hex()}  {path.name}\n"
-        with open(hash_file, 'a', encoding='utf-8') as hf:
-            hf.write(cksum_line)
+    # write hash to disk
+    bucket = path.parent
+    hash_file = bucket.parent.joinpath(bucket.name).with_suffix('.sha256')
+    cksum_line = f"{file_digest.hex()}  {path.name}\n"
+    with open(hash_file, 'a', encoding='utf-8') as hf:
+        hf.write(cksum_line)
 
-        # send response
-        self.send_response(201)
-        self.send_header("Repr-Digest", f"sha-256=:{base64.b64encode(file_digest).decode()}:")
-        self.end_headers()
+    # send response
+    return Response(status_code=201, content=None,
+                    headers={"Repr-Digest": f"sha-256=:{base64.b64encode(file_digest).decode()}:"})
 
-    def list_directory(self, path):
-        """Override for JSON instead of HTML
-
-        Return value is either a file object, or None (indicating an
-        error).  In either case, the headers are sent, making the
-        interface the same as for send_head().
-
-        """
-        # based on https://github.com/python/cpython/blob/3.13/Lib/http/server.py
-        try:
-            dir_path = pathlib.Path(path)
-        except OSError:
-            self.send_error(
-                HTTPStatus.NOT_FOUND,
-                "No permission to list directory")
-            return None
-        r = {"bucket": self.path,
-             "objects": {}}
-        for name in dir_path.iterdir():
-            if name.is_dir():
-                r['objects'][name.name] = {'directory': True,
-                                           'size': 0}
-            else:
-                r['objects'][name.name] = {'directory': False,
-                                           'size': name.stat().st_size}
-        f = io.BytesIO(json.dumps(r).encode('utf-8'))
-        length = len(f.getvalue())
-        f.seek(0)
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "application/json")
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        return f
-
-    def do_HEAD(self):
-        if self.path == '/health':
-            self.send_response(HTTPStatus.OK)
-            self.end_headers()
-            return
-        super().do_HEAD()
-
-    def healthcheck(self):
-        """Return basic info on node health"""
-        disk_stats = shutil.disk_usage(pathlib.Path(self.translate_path('/')))
-        r = {'read': True,
-             'write': True,
-             'available': disk_stats.free,
-             'percent': int(float(disk_stats.free)/float(disk_stats.total)*100.0)}
-        f = io.BytesIO(json.dumps(r).encode('utf-8'))
-        length = len(f.getvalue())
-        f.seek(0)
-        self.send_response(HTTPStatus.OK)
-        self.send_header("Content-type", "application/json")
-        self.send_header("Content-Length", str(length))
-        self.end_headers()
-        try:
-            self.copyfile(f, self.wfile)
-        finally:
-            f.close()
-
-    def do_GET(self):
-        if self.path == '/health':
-            self.healthcheck()
-            return
-        super().do_GET()
+# TODO root bucket list
+@app.get("/{bucket}/")
+def list_directory(bucket: str):
+    """List objects in bucket"""
+    try:
+        dir_path = pathlib.Path(OBJECT_DIRECTORY).joinpath(bucket)
+    except OSError as exc:
+        raise HTTPException(status_code=404) from exc
+    r = {"bucket": bucket,
+         "objects": {}}
+    for name in dir_path.iterdir():
+        if name.is_dir():
+            r['objects'][name.name] = {'directory': True,
+                                       'size': 0}
+        else:
+            r['objects'][name.name] = {'directory': False,
+                                       'size': name.stat().st_size}
+    return r
 
 
-class DirHTTPServer(ThreadingHTTPServer):
-    """An HTTP server in a particular directory"""
+#def run(server_class=DirHTTPServer, handler_class=PutHTTPRequestHandler,
+#        port=46579, directory=None):
+#    """Run this server"""
+#    server_address = ('', port)
+#    httpd = server_class(server_address, handler_class, directory=directory)
+#    httpd.serve_forever()
 
-    directory = None
+#def cli():
+#    """CLI"""
+#    parser = argparse.ArgumentParser()
+#    parser.add_argument("-d", "--directory")
+#    parser.add_argument("port", default=46579)
+#    args = parser.parse_args()
+#    run(port=int(args.port), directory=args.directory)
 
-    def __init__(self, *args, directory=None, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.directory = directory
-
-    def finish_request(self, request, client_address):
-        self.RequestHandlerClass(request, client_address, self,
-                                 directory=self.directory)
-
-
-def run(server_class=DirHTTPServer, handler_class=PutHTTPRequestHandler,
-        port=46579, directory=None):
-    """Run this server"""
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class, directory=directory)
-    httpd.serve_forever()
-
-def cli():
-    """CLI"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--directory")
-    parser.add_argument("port", default=46579)
-    args = parser.parse_args()
-    run(port=int(args.port), directory=args.directory)
-
-if __name__ == '__main__':
-    cli()
+#if __name__ == '__main__':
+#    cli()
