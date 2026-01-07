ctengel/simpler-objects

commit b443210491b61407617ef7259b5c0a35d421327b

Replication hashes and multiple FAPI bugfixes
replicate take checksum into account, maybe finishes #8

PUT doesnt require length as maybe chunked, maybe finishes #35

handle chuncked better in replicate client, see #36 for final

add head to object GETs, fixing locator

4xx and 2xx replace some 500s where possible

simpler_objects/async_replicate.py
@@ -3,35 +3,43 @@
 import argparse
 import requests
 
-TIMEOUT=1024
+TIMEOUT=2048
 
 def get_object_size(obj, skip_404=False):
     """HEAD an object to determine its size"""
-    result = requests.head(obj, timeout=1)
+    result = requests.head(obj, timeout=2)
     if skip_404 and result.status_code == 404:
-        return 0
+        return 0, None
     result.raise_for_status()
-    return int(result.headers['Content-Length'])
+    return int(result.headers['Content-Length']), result.headers['Repr-Digest']
 
 def replicate_object(source, dest):
     """Replicate one object"""
-    size = get_object_size(source)
+    size, cksum = get_object_size(source)
     assert size
-    assert not get_object_size(dest, skip_404=True)
+    assert cksum
+    assert not any(get_object_size(dest, skip_404=True))
     with requests.get(source, stream=True, timeout=TIMEOUT) as get:
         get.raise_for_status()
         assert int(get.headers['Content-Length']) == size
+        assert get.headers['Repr-Digest'] == cksum
+        # TODO Repr-Digest? Content-Length?
         put = requests.put(dest, data=get.raw, timeout=TIMEOUT,
-                           headers={'Content-Length': str(size)})
+                           headers={#'Content-Length': str(size),
+                                    'Content-Digest': cksum})#,
+#                                    'Transfer-Encoding': 'identity'})
         put.raise_for_status()
-    assert get_object_size(dest) == size
+    assert get_object_size(dest) == (size, cksum)
+    # TODO return checksum also?
     return size
 
 def get_bucket_contents(bucket):
     """Return each object in a bucket and its size"""
-    result = requests.get(bucket, timeout=5)
+    result = requests.get(bucket, timeout=10)
     result.raise_for_status()
-    return {k: v['size'] for k, v in result.json()["objects"].items() if not v['directory']}
+    return {k: (v['size'], v['checksum'])
+            for k, v in result.json()["objects"].items()
+            if not v['directory']}
 
 def replicate_bucket(source, dest):
     """Replicate any missing objects"""
@@ -41,6 +49,7 @@ def replicate_bucket(source, dest):
         if obj in dest_contents:
             assert size == dest_contents[obj]
             continue
+        # TODO check checksum also? (need to convert it)
         assert replicate_object(source + obj, dest + obj) == size
 
 def cli():

simpler_objects/locator_api.py
@@ -27,7 +27,7 @@ def get_object_server_health(url: str):
         return {'write': False, 'read': False, 'available': 0, 'percent': 0}
     return result.json()
 
-@app.get("/{bucket}/{key}")
+@app.api_route("/{bucket}/{key}", methods=["GET", "HEAD"])
 def find_object(bucket: str, key: str):
     """Return a redirect to an existing object"""
     object_path = f"{bucket}/{key}"

simpler_objects/object_server.py
@@ -62,17 +62,22 @@ def healthcheck():
          'percent': int(float(disk_stats.free)/float(disk_stats.total)*100.0)}
     return r
 
-@app.get("/{bucket}/{key}")
+@app.api_route("/{bucket}/{key}", methods=['GET', 'HEAD'])
 async def get_object(bucket: str, key: str):
     """Handle GET requests"""
     path = object_filename(bucket, key)
+    if not path.is_file():
+        raise HTTPException(status_code=404)
     my_cksum = None
-    with open(checksum_filename(path.parent), encoding='utf-8') as fp:
-        for line in fp:
-            checksum, file_name = line.strip().split()
-            if file_name == key:
-                my_cksum = bytes.fromhex(checksum)
-                break
+    try:
+        with open(checksum_filename(path.parent), encoding='utf-8') as fp:
+            for line in fp:
+                checksum, file_name = line.strip().split()
+                if file_name == key:
+                    my_cksum = bytes.fromhex(checksum)
+                    break
+    except FileNotFoundError:
+        pass
     headers = None
     if my_cksum:
         headers = {"Repr-Digest": http_digest_head(my_cksum)}
@@ -85,8 +90,8 @@ async def put_object(bucket: str, key: str, request: Request):
     # parse Content-Length
     try:
         length = int(request.headers["Content-Length"])
-    except TypeError as exc:
-        raise HTTPException(status_code=400) from exc
+    except (TypeError, KeyError):
+        length = None
 
     # ensure unique fikename
     path = object_filename(bucket, key)
@@ -101,7 +106,8 @@ async def put_object(bucket: str, key: str, request: Request):
         # TODO async? check length? lock?
         async for chunk in request.stream():
             dst.write(chunk)
-    assert path.stat().st_size == length
+    if length:
+        assert path.stat().st_size == length
 
     # Hash and compare
     file_digest = file_checksum(path)
@@ -129,10 +135,13 @@ def list_directory(bucket: str):
     r = {"bucket": bucket,
          "objects": {}}
     hashes = {}
-    with open(checksum_filename(dir_path), encoding='utf-8') as fp:
-        for line in fp:
-            checksum, file_name = line.strip().split()
-            hashes[file_name] = checksum
+    try:
+        with open(checksum_filename(dir_path), encoding='utf-8') as fp:
+            for line in fp:
+                checksum, file_name = line.strip().split()
+                hashes[file_name] = checksum
+    except FileNotFoundError:
+        pass
     for name in dir_path.iterdir():
         if name.is_dir():
             r['objects'][name.name] = {'directory': True,

commit 495b0e4187b371c994e748150418f131a213b1f5

Fixed replicator bug
Introduced in b443210491b61407617ef7259b5c0a35d421327b

Closes #8

simpler_objects/async_replicate.py
@@ -45,12 +45,13 @@ def replicate_bucket(source, dest):
     """Replicate any missing objects"""
     source_contents = get_bucket_contents(source)
     dest_contents = get_bucket_contents(dest)
+    # NOTE this "size" is a tuple that includes also a sha256
     for obj, size in source_contents.items():
         if obj in dest_contents:
             assert size == dest_contents[obj]
             continue
         # TODO check checksum also? (need to convert it)
-        assert replicate_object(source + obj, dest + obj) == size
+        assert replicate_object(source + obj, dest + obj) == size[0]
 
 def cli():
     """CLI"""